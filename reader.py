6c6606d40809d65214a08e31b3a62dc365fcf8cf
Marcin T <yahoospider@gmail.com>
2022-01-30 15:42:46 +0100

    done

--- reader.py
+++ reader.py
@@ -1,41 +1,57 @@
 import sys
 import csv
 import os
-import pickle
-import json
+# import pickle
+# import json
 
-zawartosc_pliku = []        # co w pliku
-zmiany = []                 # lista wszystkich zmian z argv 3 do n
+zawartosc_pliku = []
+ile_wierszy = 0
+
 
 def istnieje(plik):
     if os.path.exists(plik):
-        # print("ok")
+        # print("OK")
         return True
     else:
         print('NieMo pliku ale zobacz co jest w tym katalogu:')
         katalog = list(os.listdir())
-        # for jakie in katalog:
         print(katalog)
         return False
 
-istnieje(sys.argv[1])
 
-with open(sys.argv[1], "r",  newline="") as f:
+if not istnieje(sys.argv[1]):
+    sys.exit()
+
+# open file
+with open(sys.argv[1], "r", newline="") as f:
     reader = csv.reader(f)
     for line in reader:
+        ile_wierszy = ile_wierszy + 1
         zawartosc_pliku.append(line)
-        print(" ".join(line))
 
+# change file
+for zmiana in sys.argv[3:]:
+    parametry = zmiana.split(",")
+    wiersz = int(parametry[0]) - 1
 
-# .strip()
-zmiana = (sys.argv[3:])
-zmiany.append(zmiana)
-print(zmiany)
+    # rows in range ?
+    if (wiersz + 1) > ile_wierszy or (wiersz - 1) == 0 or wiersz < 0:
+        print(f'Nr wiersza po za zakresem. Min to 1, max to:', ile_wierszy)
+        sys.exit()
 
+    # columns in range?
+    kolumna = int(parametry[1]) - 1
+    max_kolumna = len(line)
+    if int(parametry[1]) > max_kolumna or int(parametry[1]) <= 0:
+        print(f'Nr kolumny po za zakresem.Min to 1, max to:', max_kolumna)
+        sys.exit()
+
+    wartosc = parametry[2]
+    zawartosc_pliku[wiersz][kolumna] = wartosc
+
+# save file
 with open(sys.argv[2], "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(zawartosc_pliku)
-    writer.writerow(zmiany)
-
-
-# delimiter =' ',quotechar =','
+    for line in zawartosc_pliku:
+        writer.writerow(line)
+        print(line)
